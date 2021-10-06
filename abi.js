c40a604117361327d61584fb3dcd53f7988ee577
champ <daschandan83333@gmail.com>
2021-10-06 15:43:52 +0530

    frontend

---
+++ abi.js
@@ -0,0 +1,820 @@
+window.abi= [
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "account",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "operator",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "bool",
+          "name": "approved",
+          "type": "bool"
+        }
+      ],
+      "name": "ApprovalForAll",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "user",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "index",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "timestamp",
+          "type": "uint256"
+        }
+      ],
+      "name": "Staked",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "operator",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256[]",
+          "name": "ids",
+          "type": "uint256[]"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256[]",
+          "name": "values",
+          "type": "uint256[]"
+        }
+      ],
+      "name": "TransferBatch",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "operator",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "value",
+          "type": "uint256"
+        }
+      ],
+      "name": "TransferSingle",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "string",
+          "name": "value",
+          "type": "string"
+        },
+        {
+          "indexed": true,
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        }
+      ],
+      "name": "URI",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "receiptId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "productId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "quantity",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "totalCost",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "buyer",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "seller",
+          "type": "address"
+        }
+      ],
+      "name": "newOrder",
+      "type": "event"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "productId",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "price",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "quantity",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "address",
+          "name": "seller",
+          "type": "address"
+        }
+      ],
+      "name": "newProduct",
+      "type": "event"
+    },
+    {
+      "inputs": [],
+      "name": "Admin",
+      "outputs": [
+        {
+          "internalType": "address payable",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [],
+      "name": "Owner",
+      "outputs": [
+        {
+          "internalType": "address payable",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "Products",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "name",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "price",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "quantity",
+          "type": "uint256"
+        },
+        {
+          "internalType": "address payable",
+          "name": "seller",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "Receipts",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "productId",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "quantity",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "totalCost",
+          "type": "uint256"
+        },
+        {
+          "internalType": "address payable",
+          "name": "buyer",
+          "type": "address"
+        },
+        {
+          "internalType": "address payable",
+          "name": "seller",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [],
+      "name": "Stake",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function",
+      "payable": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "account",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        }
+      ],
+      "name": "balanceOf",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address[]",
+          "name": "accounts",
+          "type": "address[]"
+        },
+        {
+          "internalType": "uint256[]",
+          "name": "ids",
+          "type": "uint256[]"
+        }
+      ],
+      "name": "balanceOfBatch",
+      "outputs": [
+        {
+          "internalType": "uint256[]",
+          "name": "",
+          "type": "uint256[]"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "account",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        }
+      ],
+      "name": "burnNFT",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "buyerReceipt",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "account",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "operator",
+          "type": "address"
+        }
+      ],
+      "name": "isApprovedForAll",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "account",
+          "type": "address"
+        },
+        {
+          "internalType": "bytes",
+          "name": "description",
+          "type": "bytes"
+        }
+      ],
+      "name": "mintNFT",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "productId",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [],
+      "name": "receiptId",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256[]",
+          "name": "ids",
+          "type": "uint256[]"
+        },
+        {
+          "internalType": "uint256[]",
+          "name": "amounts",
+          "type": "uint256[]"
+        },
+        {
+          "internalType": "bytes",
+          "name": "data",
+          "type": "bytes"
+        }
+      ],
+      "name": "safeBatchTransferFrom",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "internalType": "bytes",
+          "name": "data",
+          "type": "bytes"
+        }
+      ],
+      "name": "safeTransferFrom",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "sellerReceipt",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "operator",
+          "type": "address"
+        },
+        {
+          "internalType": "bool",
+          "name": "approved",
+          "type": "bool"
+        }
+      ],
+      "name": "setApprovalForAll",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "bytes4",
+          "name": "interfaceId",
+          "type": "bytes4"
+        }
+      ],
+      "name": "supportsInterface",
+      "outputs": [
+        {
+          "internalType": "bool",
+          "name": "",
+          "type": "bool"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "uri",
+      "outputs": [
+        {
+          "internalType": "string",
+          "name": "",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "string",
+          "name": "_name",
+          "type": "string"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_price",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_quantity",
+          "type": "uint256"
+        }
+      ],
+      "name": "listItem",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_productId",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "_quantity",
+          "type": "uint256"
+        }
+      ],
+      "name": "buy",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function",
+      "payable": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_receiptId",
+          "type": "uint256"
+        }
+      ],
+      "name": "orderAccept",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function",
+      "payable": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_receiptId",
+          "type": "uint256"
+        }
+      ],
+      "name": "orderReject",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function",
+      "payable": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_receiptId",
+          "type": "uint256"
+        }
+      ],
+      "name": "itemDelivered",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_receiptId",
+          "type": "uint256"
+        }
+      ],
+      "name": "dispute",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function",
+      "payable": true
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "_receiptId",
+          "type": "uint256"
+        },
+        {
+          "internalType": "address payable",
+          "name": "_justice",
+          "type": "address"
+        }
+      ],
+      "name": "resolve",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function",
+      "payable": true
+    }
+  ],
+
+  stake.abi= [
+    {
+      "inputs": [],
+      "stateMutability": "nonpayable",
+      "type": "constructor"
+    },
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": true,
+          "internalType": "address",
+          "name": "user",
+          "type": "address"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "amount",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "index",
+          "type": "uint256"
+        },
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "timestamp",
+          "type": "uint256"
+        }
+      ],
+      "name": "Staked",
+      "type": "event"
+    },
+    {
+      "inputs": [],
+      "name": "Admin",
+      "outputs": [
+        {
+          "internalType": "address payable",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function",
+      "constant": true
+    },
+    {
+      "inputs": [],
+      "name": "Stake",
+      "outputs": [],
+      "stateMutability": "payable",
+      "type": "function",
+      "payable": true
+    }
+  ]
--- main.js
+++ main.js
@@ -5,6 +5,7 @@ Moralis.serverURL = 'https://uvoj28qnh9dj.moralishost.com:2053/server'
 init = async() => {
     hideElement(userInfo);
     hideElement(listItemForm);
+    hideElement(userStakeForm);
     window.web3 = await Moralis.Web3.enable();
     initUser();
 }
@@ -14,11 +15,13 @@ initUser = async()=>{
         hideElement(userConnectButton);
         showElement(userProfileButton);
         showElement(openListItemButton);
+        hideElement(userStakeForm);
     }
     else{
         showElement(userConnectButton);
         hideElement(userProfileButton);
         hideElement(openListItemButton);
+        hideElement(userStakeForm);
     }
 }
 
@@ -59,6 +62,14 @@ openUserInfo = async () =>{
         else{
             hideElement(userAvatarImg);
         }
+        const userAddress= user.get('address');
+        if(userAddress){
+            userAddressField.value= userAddress;
+            
+        }
+        else{
+            userAddressField.value="";
+        }
 
         showElement(userInfo);
     }
@@ -70,6 +81,7 @@ openUserInfo = async () =>{
 saveUserInfo = async () => {
     user.set('email', userEmailField.value);
     user.set('username', userUsernameField.value);
+    user.set('address', userAddressField.value);
 
     if(userAvatarFile.files.length>0){
         const avatar =new Moralis.File("avatar.jpg", userAvatarFile.files[0]);
@@ -78,6 +90,27 @@ saveUserInfo = async () => {
     await user.save();
     alert ("User info saved successfully!");
     openUserInfo();
+}
+
+userStake= async()=>{
+    if (stakeAmount.value<0.05){
+        alert("Please stake more than or equal to 0.05 ether!");
+        return;
+    }
+
+    //solidity and web3 integration
+    async function stake_Amount(){
+    let _amount= stakeAmount.value*Math.pow(10,18);
+    window.web3 = await Moralis.Web3.enable();
+    let contractInstance = new web3.eth.Contract(stake.abi, "0xeF37E296d3f58E35ED0c1B6959AF29fB2990F568")
+    contractInstance.methods.stake().send({from: ethereum.selectedAddress, value: _amount})
+    .on('receipt', function(receipt){
+        console.log(receipt);
+        alert(receipt.events.Staked.returnValues);
+    })
+  }
+  stake_Amount();
+  
 }
 
 listItem= async()=>{
@@ -90,38 +123,56 @@ listItem= async()=>{
         return;
     }
     
-const itemFile = new Moralis.File("itemFile.jpg", listItemFile.files[0])
-await itemFile.saveIPFS();
+    const itemFile = new Moralis.File("itemFile.jpg", listItemFile.files[0])
+    await itemFile.saveIPFS();
 
-const itemFilePath= itemFile.ipfs();
-const itemFileHash=itemFile.hash();
+    const itemFilePath= itemFile.ipfs();
+    const itemFileHash=itemFile.hash();
 
-const metadata={
-    name: listItemNameField.value,
-    description: listItemDescriptionField.value,
-    itemFilePath: itemFilePath,
-    itemFileHash: itemFileHash
-};
+    const metadata={
+     name: listItemNameField.value,
+     description: listItemDescriptionField.value,
+     price: listItemPriceField.value,
+     itemFilePath: itemFilePath,
+     itemFileHash: itemFileHash
+    };
 
 
-const itemFileMetadata = new Moralis.File("metadata.json", {base64 : btoa(JSON.stringify(metadata))});
-await itemFileMetadata.saveIPFS();
+  const itemFileMetadata = new Moralis.File("metadata.json", {base64 : btoa(JSON.stringify(metadata))});
+  await itemFileMetadata.saveIPFS();
 
-const itemFileMetadataPath= itemFileMetadata.ipfs();
-const itemFileMetadataHash= itemFileMetadata.hash();
+  const itemFileMetadataPath= itemFileMetadata.ipfs();
+  const itemFileMetadataHash= itemFileMetadata.hash();
 
-const Item = Moralis.Object.extend("Item");
+  const Item = Moralis.Object.extend("Item");
 
-//Create a new instance of that class.
-const item =new Item();
-item.set('name', listItemNameField.value);
-item.set('description', listItemDescriptionField.value);
-item.set('itemFilePath', itemFilePath);
-item.set('itemFileHash', itemFileHash);
-item.set('metadataFilePath', itemFileMetadataPath);
-item.set('metadataFileHash', itemFileMetadataHash);
-await item.save();
-console.log(item);
+  //Create a new instance of that class.
+  const item =new Item();
+  item.set('name', listItemNameField.value);
+  item.set('description', listItemDescriptionField.value);
+  item.set('price', listItemPriceField.value);
+  item.set('itemFilePath', itemFilePath);
+  item.set('itemFileHash', itemFileHash);
+  item.set('metadataFilePath', itemFileMetadataPath);
+  item.set('metadataFileHash', itemFileMetadataHash);
+  await item.save();
+  console.log(item);
+
+  //solidity and web3 integration
+  async function list_Item(){
+    let _name= listItemNameField.value;
+    let _price= listItemPriceField.value;
+    let _quantity= listItemQuantityField.value;
+    window.web3 = await Moralis.Web3.enable();
+    let contractInstance = new web3.eth.Contract(window.abi, "0xcdEc06999de5EEF40DA3542D8ccc29062bbe037b")
+    contractInstance.methods.listItem(_name, _price, _quantity).send({from: ethereum.selectedAddress})
+    .on('receipt', function(receipt){
+        console.log(receipt);
+        alert(receipt.events.newProduct.returnValues);
+    })
+  }
+  list_Item();
+  
 }
 
 hideElement = (element) => element.style.display = "none";
@@ -137,10 +188,14 @@ userProfileButton.onclick= openUserInfo;
 const openListItemButton= document.getElementById("btnOpenListItem");
 openListItemButton.onclick = () =>showElement(listItemForm);
 
+const openStakeFormButton= document.getElementById("btnStakeForm");
+openStakeFormButton.onclick = () =>showElement(userStakeForm);
+
 //User Profile
 const userInfo= document.getElementById("userInfo");
 const userUsernameField= document.getElementById("txtUsername");
 const userEmailField = document.getElementById("txtEmail");
+const userAddressField =document.getElementById("txtAddress")
 const userAvatarImg = document.getElementById("imgAvatar");
 const userAvatarFile= document.getElementById("fileAvatar");
 
@@ -148,16 +203,50 @@ document.getElementById("btnCloseUserInfo").onclick = () => hideElement(userInfo
 document.getElementById("btnLogout").onclick=logout; 
 document.getElementById("btnSaveUserInfo").onclick=saveUserInfo; 
 
+//Stake
+const userStakeForm= document.getElementById("userStakeForm")
+const stakeAmount= document.getElementById("numStakeAmount");
+document.getElementById("btnCloseStakeForm").onclick=() => hideElement(userStakeForm)
+document.getElementById("btnStakeConfirm").onclick= userStake; 
+
 //List Item
-const listItemForm= document.getElementById("listItem");
+const listItemForm= document.getElementById("listItemForm");
 
 const listItemNameField= document.getElementById("txtListItemName");
 const listItemDescriptionField= document.getElementById("txtListItemDescription");
 const listItemPriceField= document.getElementById("numListItemPrice");
+const listItemQuantityField= document.getElementById("numListItemQuantity");
+const listItemTypeField= document.getElementById("selectListItemType");
 const listItemStatusField= document.getElementById("selectListItemStatus");
 const listItemFile= document.getElementById("fileListItem");
 document.getElementById("btnCloseListItem").onclick= () => hideElement(listItemForm); 
 document.getElementById("btnListItem").onclick= listItem; 
 
+// Burger menu 
+
+const burger = document.querySelector('.burger');
+const menuNav = document.querySelector('.menu');
+const burgerLine = document.querySelector('.burger__line');
+
+if (burger) {
+    burger.addEventListener('click', (e) => {
+        document.body.classList.toggle('_lock');
+        e.currentTarget.classList.toggle('burger--active');
+        burgerLine.classList.toggle('burger__line--active');
+        menuNav.classList.toggle('menu--active');
+    });
+}
 
 init();
+
+
+
+
+
+  function on() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function off() {
+    document.getElementById("overlay").style.display = "none";
+  }
